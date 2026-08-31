a393c7b8504c1d9a6958fd31119450fb14f58193
saranyaloganathan007-ship-it <saranyaloganathan007@gmail.com>
2026-08-31 19:17:07 +0530

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+student = {
+    "name": "Saranya",
+    "course": "Information Technology",
+    "skill": "Python"
+}
+
+print("Name:", student["name"])
+print("Course:", student["course"])
+print("Skill:", student["skill"])
