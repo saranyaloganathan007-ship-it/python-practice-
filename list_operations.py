aa11d14e1880016077a163574da57eaefaec9d5d
saranyaloganathan007-ship-it <saranyaloganathan007@gmail.com>
2026-08-31 19:24:55 +0530

    Create list_operations.py

---
+++ list_operations.py
@@ -0,0 +1,9 @@
+numbers = [10, 20, 30, 40, 50]
+
+numbers.append(60)
+numbers.remove(20)
+
+print("List:", numbers)
+print("Length:", len(numbers))
+print("Maximum:", max(numbers))
+print("Minimum:", min(numbers))
