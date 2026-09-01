b40587f596fe3cff7d27729f24387aacce48d9ae
saranyaloganathan007-ship-it <saranyaloganathan007@gmail.com>
2026-09-01 19:30:43 +0530

    Create exception_handling.py

---
+++ exception_handling.py
@@ -0,0 +1,12 @@
+try:
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+
+    result = a / b
+    print("Result:", result)
+
+except ValueError:
+    print("Please enter numbers only")
+
+except ZeroDivisionError:
+    print("Cannot divide by zero")
