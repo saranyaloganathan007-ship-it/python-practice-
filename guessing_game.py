eb47a1b2444f856cead50db4218a709ad00600f2
saranyaloganathan007-ship-it <saranyaloganathan007@gmail.com>
2026-09-04 18:39:49 +0530

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -2,10 +2,17 @@ import random
 
 secret = random.randint(1, 10)
 
-guess = int(input("Guess a number from 1 to 10: "))
+for attempt in range(3):
+    guess = int(input("Guess a number from 1 to 10: "))
 
-if guess == secret:
-    print("Correct! 🎉")
+    if guess == secret:
+        print("Correct! ")
+        break
+    elif guess < secret:
+        print("Too low!")
+    else:
+        print("Too high!")
 else:
-    print("Wrong guess!")
+    print("Better luck next time!")
     print("The number was:", secret)
+
